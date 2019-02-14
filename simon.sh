826ff42448330679742d4ae017e70b677f003ce4
Maxim Zaitsev <7aitsev@pm.me>
2019-02-14 18:12:21 +0300

    Closes #1

--- simon.sh
+++ simon.sh
@@ -7,48 +7,65 @@
 # 3. run the script (if it is the first run, then you'll be prompted just to create a snapshot of the site
 #     - file called "simon.old") and follow the prompts.
 
-SNAPSHOT_OLD="simon.old"
-BASE="http://simonstalenhag.se/"
-SNAPSHOT_NEW=$(curl -ks $BASE | sed -e 's/.$//;/^$/d;s/^[[:space:]]*//;s/[[:space:]]*$//')
+SNAPSHOT_OLD='simon.old'
+BASE='http://simonstalenhag.se/'
+#SNAPSHOT_NEW=$(curl -ks "$BASE" | sed -e 's/.$//;/^$/d;s/^[[:space:]]*//;s/[[:space:]]*$//')
+SNAPSHOT_NEW=$(sed -e 's/.$//;/^$/d;s/^[[:space:]]*//;s/[[:space:]]*$//' <"simon.new")
 
 # overwrite the old snapshot?
 askOverwrite() {
+    printf '\n'
     while true ; do
-        read -p "Overwrite the old snapshot (y/n)? " yn
-        case $yn in
-            [Yy]* ) echo -n "$SNAPSHOT_NEW" >$SNAPSHOT_OLD; break;;
-            [Nn]* ) break;;
-            *) echo "Please answer yes or no ";;
+        read -p 'Overwrite the old snapshot (y/n)? ' yn
+        case "$yn" in
+            [Yy]* )
+                printf '%s' "$SNAPSHOT_NEW" >"$SNAPSHOT_OLD"
+                printf 'Snapshot overwritten'
+                break;;
+            [Nn]* )
+                printf 'Snapshot untouched';
+                break;;
+            * )
+                printf 'Please answer yes or no\n';;
         esac
     done
 }
 
 # fetch link(s) from diff and download pic(s)
 fetchNDownload() {
-    _links=$(echo "$1" | grep -E "^>" | cut -d '"' -f 2 | grep ".jpg" | sort -u)
+    _links=$(printf '%s' "$1" \
+        | grep -E '^>' | cut -d '"' -f 2 | grep -i '.jpe\?g' | sort -u)
+    printf '\n'
     if [ -n "$_links" ]
         then
             # show a list of fetched link(s)
-            printf '\nFetched links:\n%s\n' "$_links"
+            printf 'Fetched links:\n%s\n\n' "$_links"
             for link in $_links; do
-                _fname=$(echo $link | cut -d '/' -f 2)
-                # ask if user wishes to save file by the fetched link
+                _fname=$(printf '%s\n' "$link" | cut -d '/' -f 2)
+                # ask if a user wishes to save the file from the fetched link
                 while true; do
                     read -p "Save \"$_fname\" (y/n)? " yn
-                    case $yn in
-                        [Yy]* ) wget -nv $BASE$link; break;;
+                    case "$yn" in
+                        [Yy]* )
+                            wget -nv "$BASE$link";
+                            break;;
 # -P prefix
 # --directory-prefix=prefix
 #   Set directory prefix to prefix.  The directory prefix is the directory where all other files and subdirectories will be
 #   saved to, i.e. the top of the retrieval tree.  The default is . (the current directory).
-                        [Nn]* ) break;;
-                        * ) echo "Please answer yes or no";;
+                        [Nn]* )
+                            #printf 'Skipping...'
+                            break;;
+                        * )
+                            printf 'Please answer yes or no\n';;
                     esac
                 done
             done
         else
-            echo "Snapshots are different, but no links fetched..."
-            echo "$1"
+            printf 'No links fetched...\n\n'
+            printf -- '-----BEGIN DIFF BLOCK-----\n'
+            printf '%s\n' "$1"
+            printf -- '-----END DIFF BLOCK-----\n'
     fi
     askOverwrite
 }
@@ -56,21 +73,23 @@ fetchNDownload() {
 # is there something new?
 findDiffs() {
     _diffs=$(printf '%s' "$SNAPSHOT_NEW" | diff "$SNAPSHOT_OLD" -)
+    printf '\n'
     if [ -n "$_diffs" ]
         then
+            printf 'Snapshots are different\n'
             fetchNDownload "$_diffs"
         else
-            echo "Snapshots are the same"
+            printf 'Snapshots are the same\n'
     fi
 }
 
 # is there simon.old file?
 if [ -f "$SNAPSHOT_OLD" ]
     then
-        echo "Snapshot \"$SNAPSHOT_OLD\" found"
+        printf 'Snapshot "%s" found\n' "$SNAPSHOT_OLD"
         findDiffs
     else
-        echo "Snapshot \"$SNAPSHOT_OLD\" not found"
-        echo -n "$SNAPSHOT_NEW" > $SNAPSHOT_OLD
-        echo "Snapshot of the site saved to \"$SNAPSHOT_OLD\""
+        printf 'Snapshot "%s" not found\n' "$SNAPSHOT_OLD"
+        printf '%s' "$SNAPSHOT_NEW" >"$SNAPSHOT_OLD"
+        printf 'Snapshot of the site saved as "%s"\n' "$SNAPSHOT_OLD"
 fi
